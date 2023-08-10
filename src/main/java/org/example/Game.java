3628e49ed55bf71008d08cf5517263141af7745f
Ruslan-Shev <134603263+Ruslan-Shev@users.noreply.github.com>
2023-08-10 20:38:30 +0300

    Game.java

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,32 +3,29 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-class Game {
-    List<org.example.Player> players;
+public class Game {
+    private final List<Player> players;
 
     public Game() {
         players = new ArrayList<>();
     }
 
-    public void register(org.example.Player player) {
+    public void register(Player player) {
         players.add(player);
     }
 
-    public int round(String playerName1, String playerName2) throws NotRegisteredException {
-        org.example.Player player1 = null;
-        org.example.Player player2 = null;
-
-        for (org.example.Player player : players) {
-            if (player.name.equals(playerName1)) {
-                player1 = player;
-            } else if (player.name.equals(playerName2)) {
-                player2 = player;
+    private Player findPlayerByName(String name) throws NotRegisteredException {
+        for (Player player : players) {
+            if (player.name.equals(name)) {
+                return player;
             }
         }
+        throw new NotRegisteredException("Player not registered");
+    }
 
-        if (player1 == null || player2 == null) {
-            throw new NotRegisteredException("One or both players are not registered");
-        }
+    public int round(String playerName1, String playerName2) throws NotRegisteredException {
+        Player player1 = findPlayerByName(playerName1);
+        Player player2 = findPlayerByName(playerName2);
 
         if (player1.strength > player2.strength) {
             return 1;
